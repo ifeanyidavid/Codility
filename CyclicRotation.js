0541486afe4c1615a4f3261cea8a98c6dc771c97
Dave Kalu <davekalu46@gmail.com>
2020-01-16 09:32:28 +0100

    cyclic rotation refactor

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -1,26 +1,32 @@
-function cyclicRotation(array, k) {
-  if (Number.isInteger(k) && Array.isArray(array)) {
-    if (k > 0 && k <= 100 && array.length > 0 && array.length <= 100) {
-      const minValue = Math.min.apply(null, array);
-      const maxValue = Math.max.apply(null, array);
-      if (minValue >= -1000 && maxValue <= 1000) {
-        let cyclicArray = [];
-        if (array.length == k) return array;
-        for (let i = 0; i < k; i++) {
-          // Pop last element of the array
-          let lastElement = array.pop();
-          // Add the popped value to the begining of the array
-          arr.unshift(lastElement);
-          cyclicArray = array;
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+      if (Number.isInteger(K) && Array.isArray(A)) {
+        if (K > 0 && K <= 100 && A.length > 0 && A.length <= 100) {
+          const minValue = getMinValueFromArray(A);
+          const maxValue = getMaxValueFromArray(A);
+          
+          if(minValue >= -1000 && maxValue <= 1000){
+              let cyclicArray = [];
+              if (A.length == K) return A;
+                  for (let i = 0; i < K; i++) {
+                    // Pop last element of the array
+                    let lastElement = A.pop();
+                    // Add the popped value to the begining of the array
+                    A.unshift(lastElement);
+                    cyclicArray = A;
+                  }
+              return cyclicArray;
+          }
+          return A;
         }
-        return cyclicArray;
-      }
-      return array;
+        return A;
     }
-    return array;
-  }
-  return "Invalid arguments";
 }
 
-const arr = [-4];
-console.log(cyclicRotation(arr, 0));
+function getMaxValueFromArray(A){
+    return Math.max.apply(null, A)
+}
+
+function getMinValueFromArray(A){
+   return Math.min.apply(null, A)
+}
