18dc38d50b53de7e5bb043a31311bb108af7031d
Dave Kalu <davekalu46@gmail.com>
2020-01-17 11:42:35 +0100

    Updates

--- TapeEquilibrium.js
+++ TapeEquilibrium.js
@@ -1,23 +1,30 @@
-const A = [1, 3];
+const A = [1, 2, 3, 4, 5];
 
-function solution(A) {
+// Slower solution
+function solution1(A) {
   if (Array.isArray(A)) {
     const totalSumValues = [];
-    if (A.length == 0) return 0;
-    if (A.length == 1) return A[0];
-    for (let i = 1; i < A.length; i++) {
-      const tempArray = [...A];
+    const N = A.length;
+    const minRange = 2;
+    const maxRange = 100000;
 
-      const splitTempArray = tempArray.splice(0, i);
+    if (isWithinRange(minRange, maxRange, N, N)) {
+      if (N == 0) return 0;
+      if (N == 1) return A[0];
 
-      const tempArraySum = arraySum(tempArray);
-      const splitTempArraySum = arraySum(splitTempArray);
+      for (let i = 1; i < A.length; i++) {
+        const tempArray = [...A];
+        const splitTempArray = tempArray.splice(0, i);
+        const tempArraySum = arraySum(tempArray);
+        const splitTempArraySum = arraySum(splitTempArray);
+        const arraySumDifference = Math.abs(splitTempArraySum - tempArraySum);
 
-      const arraySumDifference = Math.abs(splitTempArraySum - tempArraySum);
-
-      totalSumValues.push(arraySumDifference);
+        totalSumValues.push(arraySumDifference);
+      }
+      return getMinValueFromArray(totalSumValues);
+    } else {
+      return "Invalid arguments";
     }
-    return getMinValueFromArray(totalSumValues);
   } else {
     return "Invalid arguments";
   }
@@ -34,4 +41,27 @@ function getMinValueFromArray(A) {
   return A.reduce((min, v) => (min <= v ? min : v), Infinity);
 }
 
-console.log(solution(A));
+function isWithinRange(minRange, maxRange, minValue, maxValue) {
+  return minValue >= minRange && maxValue <= maxRange;
+}
+
+// Faster solution
+function solution2(A) {
+  // write your code in JavaScript (Node.js 6.4.0)
+  var sum1 = 0;
+  var sum2 = A.reduce(function(acc, i) {
+    return acc + i;
+  });
+
+  var minDiff = Number.POSITIVE_INFINITY;
+
+  for (var p = 0; p < A.length - 1; p++) {
+    sum1 += A[p];
+    sum2 -= A[p];
+
+    var diff = sum2 - sum1;
+    minDiff = Math.min(minDiff, Math.abs(diff));
+  }
+
+  return minDiff;
+}
