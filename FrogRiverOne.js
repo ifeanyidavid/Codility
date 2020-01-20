20b7bda43fecf9897f3e2b4856c14dcb85ad370d
Dave Kalu <davekalu46@gmail.com>
2020-01-20 17:10:12 +0100

    Implemented missing elements function

--- FrogRiverOne.js
+++ FrogRiverOne.js
@@ -10,6 +10,20 @@ function getMaxValueFromArray(A) {
   return A.reduce((max, v) => (max >= v ? max : v), -Infinity);
 }
 
+function getMinValueFromArray(A) {
+  return A.reduce((min, v) => (min <= v ? min : v), Infinity);
+}
+
+function countElements(A) {
+  const count = new Array(A.length + 1).fill(0);
+  for (let i = 0; i < A.length; i++) {
+    count[A[i]] += 1;
+  }
+  console.log(count);
+
+  return count;
+}
+
 function solution(X, A) {
   if (isInteger(X) && Array.isArray(A)) {
     const minRange = 1;
@@ -20,6 +34,12 @@ function solution(X, A) {
       isWithinRange(minRange, maxRange, X, X)
     ) {
       if (getMaxValueFromArray(A) === X) {
+        if (
+          A.length > 1 &&
+          getMinValueFromArray(A) === getMaxValueFromArray(A)
+        ) {
+          return -1;
+        }
         if (A.indexOf(X) !== -1) {
           return A.indexOf(X);
         } else {
@@ -32,7 +52,7 @@ function solution(X, A) {
   }
 }
 
-const A = [1];
-const X = 1;
+const A = [1, 2, 3, 5, 3, 1];
+const X = 5;
 
 console.log(solution(X, A));
--- countingElements.js
+++ countingElements.js
@@ -4,9 +4,30 @@ function counting(A) {
   for (let i = 0; i < A.length; i++) {
     count[A[i]] += 1;
   }
+
   return count;
 }
 
-const A = [1, 3, 4, 5, 3, 5];
+function sort(A) {
+  return Array.from(new Set(A.sort()));
+}
 
-console.log(counting(A));
+function findMissingElements(A) {}
+
+const A = [7, 0, 2, 3, 5, 4, 6, 6];
+
+var mia = sort(A).reduce(function(acc, cur, ind, arr) {
+  var diff = cur - arr[ind - 1];
+  if (diff > 1) {
+    var i = 1;
+    while (i < diff) {
+      acc.push(arr[ind - 1] + i);
+      i++;
+    }
+  }
+  return acc;
+}, []);
+
+console.log(mia);
+
+// console.log(sort(A));
